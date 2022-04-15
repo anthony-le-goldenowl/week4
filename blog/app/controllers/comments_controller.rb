cfc938756eccbffadf716c792d0854fbddcd4c76
anthony-le-goldenowl <anthony.le.goldenowl@gmail.com>
2022-04-15 16:15:16 +0700

    4/15/2022

---
+++ blog/app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def index
+    @article = Article.find(params[:article_id])
+  end
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+  def show
+    @article = Article.find(params[:article_id])
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
+
