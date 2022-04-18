ac7ae9be66b7196ca83ef12db6a3cb4972dd06fb
anthony-le-goldenowl <anthony.le.goldenowl@gmail.com>
2022-04-18 17:02:02 +0700

    4/18/2022

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   def index
     @article = Article.find(params[:article_id])
   end
@@ -7,13 +8,19 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
   def show
     @article = Article.find(params[:article_id])
   end
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 end
 
